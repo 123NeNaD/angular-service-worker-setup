c0908e26d73e2afc3d52f41174835e7c63462092
123NeNaD <61844304+123NeNaD@users.noreply.github.com>
2023-02-01 09:14:32 +0100

    Adding Advanced Service Worker Logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-service-worker-setup';
+  public title = 'angular-service-worker-setup';
+
+  constructor(private swUpdate: SwUpdate) {
+    this.checkForNewVersion();
+
+    // Check for new version every minute
+    setInterval(() => this.checkForNewVersion(), 60 * 1000);
+  }
+
+  checkForNewVersion = async () => {
+    try {
+      // Check if Service Worker is supported by the Browser
+      if (this.swUpdate.isEnabled) {
+        // Check if new version is available
+        const isNewVersion = await this.swUpdate.checkForUpdate();
+        if (isNewVersion) {
+          // Check if new version is activated
+          const isNewVersionActivated = await this.swUpdate.activateUpdate();
+          // Reload the application with new version if new version is activated
+          if (isNewVersionActivated) window.location.reload();
+        }
+      }
+    } catch (error) {
+      console.log(
+        `Service Worker - Error when checking for new version of the application: `,
+        error
+      );
+      window.location.reload();
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      // or after 10 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:10000'
     })
   ],
   providers: [],
